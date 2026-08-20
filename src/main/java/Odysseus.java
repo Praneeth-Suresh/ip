e15e2d9e9650dfbbf612bad7fed4fa5e67bf5997
Praneeth Suresh <praneeth.suresh.s@gmail.com>
2026-08-20 16:14:12 +0800

    Add Odysseus startup character art

--- src/main/java/Odysseus.java
+++ src/main/java/Odysseus.java
@@ -1,6 +1,10 @@
 public class Odysseus {
     public static void main(String[] args) {
-        String banner = "Odysseus\n";
+        String banner = "  ___    ____  __   __  ____   ____  _____  _   _  ____\n"
+                + " / _ \\  |  _ \\ \\ \\ / / / ___| / ___|| ____|| | | |/ ___|\n"
+                + "| | | | | | | | \\ V /  \\___ \\ \\___ \\|  _|  | | | |\\___ \\\n"
+                + "| |_| | | |_| |  | |    ___) | ___) | |___ | |_| | ___) |\n"
+                + " \\___/  |____/   |_|   |____/ |____/|_____| \\___/ |____/\n";
         System.out.println(banner);
     }
 }
